c33186d437f88e9d8e6041dc272be1ed8831ae8c
gefiranch <Gefiranch@gmail.com>
2025-06-09 20:30:47 +0700

    Update Controller.class.php

--- controller/Controller.class.php
+++ controller/Controller.class.php
@@ -1,1 +1,23 @@
+<?php
+class Controller
+{
 
+    function model($model)
+    {
+        require_once("./model/Model.class.php");
+        require_once("./model/$model.class.php");
+        return new $model();
+    }
+
+    function view($view, $data = [])
+    {
+        foreach ($data as $key => $value) {
+            $$key = $value;
+        }
+        if (file_exists("./view/$view")) {
+            include("./view/$view");
+        } else {
+            echo "View '$view' tidak ditemukan.";
+        }
+    }
+}
