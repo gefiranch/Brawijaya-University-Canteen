2ac5607f50657118a922866230d39417c43f96b5
gefiranch <Gefiranch@gmail.com>
2025-06-09 22:50:10 +0700

    Update CanteenModel.class.php

--- model/CanteenModel.class.php
+++ model/CanteenModel.class.php
@@ -1,1 +1,23 @@
+<?php
+
+class CanteenModel extends Model
+{
+    public function getAllCanteens()
+    {
+        $query = "SELECT * FROM canteen";
+        $result = $this->db->query($query);
+        return $result->fetch_assoc();
+    }
+
+    public function getCanteenById($id)
+    {
+        $query = "SELECT * FROM canteen WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_assoc();
+    }
+}
 
