adf26f5bc62d0942c2fde50e4c37f60e9cc95998
gefiranch <Gefiranch@gmail.com>
2025-06-10 07:47:26 +0700

    Update CanteensModel.class.php

--- model/CanteensModel.class.php
+++ model/CanteensModel.class.php
@@ -1,23 +1,31 @@
 <?php
 
-class CanteenModel extends Model
+require_once 'model/Model.class.php';
+
+class CanteensModel extends Model
 {
-    public function getAllCanteens()
+    // Mengambil semua data kantin
+    public function getAllCanteen()
     {
         $query = "SELECT * FROM canteen";
         $result = $this->db->query($query);
-        return $result->fetch_assoc();
+
+        $canteens = [];
+        while ($row = $result->fetch_assoc()) {
+            $canteens[] = $row;
+        }
+        return $canteens;
     }
 
+    // Mengambil data kantin berdasarkan ID
     public function getCanteenById($id)
     {
-        $query = "SELECT * FROM canteen WHERE id = ?";
+        $query = "SELECT * FROM canteen WHERE id_canteen = ?";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('i', $id);
+        $stmt->bind_param("i", $id);
         $stmt->execute();
-        $result = $stmt->get_result();
 
+        $result = $stmt->get_result();
         return $result->fetch_assoc();
     }
 }
-
