d4c409e9f4ea2a1a198d3fb29b5218855a84adbf
gefiranch <Gefiranch@gmail.com>
2025-06-13 20:07:57 +0700

    Update canteenList.php

--- view/canteenList.php
+++ view/canteenList.php
@@ -68,8 +68,6 @@
         </div>
     </main>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
