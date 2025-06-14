6593018a3315fbc263fb6c5a7052b5ebcb0d02a2
Zahra Nurul Haniyyah Anas <xhyblyz@gmail.com>
2025-06-15 00:46:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 $c = $_GET['c'] ?? 'Users'; // ambil controller dari URL, default ke 'home'
 $m = $_GET['m'] ?? 'login'; // ambil method dari URL, default ke 'index'
 
-require_once 'controllers/Controller.class.php'; // include file controller
-require_once 'controllers/' . $c . '.class.php'; // include file controller
+require_once 'controller/Controller.class.php'; // include file controller
+require_once 'controller/' . $c . '.class.php'; // include file controller
 
 // Run!
 $controller = new $c(); // inisialisasi controller
