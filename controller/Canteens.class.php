8cc745d47ed3985f3e3e01e6c576ed871e8f512e
gefiranch <Gefiranch@gmail.com>
2025-06-15 06:48:59 +0700

    Update Canteens.class.php

--- controller/Canteens.class.php
+++ controller/Canteens.class.php
@@ -1,1 +1,29 @@
+<?php
 
+require_once '../controller/Controller.class.php';
+require_once '../model/CommentsModel.class.php';
+
+class Canteens extends Controller
+
+{
+    // Menampilkan daftar semua kantin
+    public function listCanteens()
+    {
+        $canteensModel = new CanteensModel();
+        $canteens = $canteensModel->getAllCanteen();
+
+        include '/view/canteenList.php';
+    }
+
+    // Menampilkan detail kantin berdasarkan ID
+    public function viewDetail($id)
+    {
+        $canteensModel = new CanteensModel();
+        $canteen = $canteensModel->getCanteenById($id);
+
+        $commentsModel = new CommentsModel();
+        $comments = $commentsModel->getCommentsByCanteenId($id);
+
+        include '/view/canteenDetail.php';
+    }
+}
