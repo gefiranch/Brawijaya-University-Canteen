b423a7a2b350cd208957004702d086e1246bf789
gefiranch <Gefiranch@gmail.com>
2025-06-14 20:59:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $c = $_GET['c'] ?? 'Users'; // ambil controller dari URL, default ke 'home'
 $m = $_GET['m'] ?? 'login'; // ambil method dari URL, default ke 'index'
 
 require_once 'controllers/Controller.class.php'; // include file controller
-require_once 'controllers/' . $c . 'class.php'; // include file controller
+require_once 'controllers/' . $c . '.class.php'; // include file controller
 
 // Run!
 $controller = new $c(); // inisialisasi controller
