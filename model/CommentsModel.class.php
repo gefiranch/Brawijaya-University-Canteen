7847a95d93ac1248b30e86152c4609cd4768e550
azkamitsa <azkamitsa@gmail.com>
2025-06-21 21:29:41 +0700

    Update CommentsModel.class.php

--- model/CommentsModel.class.php
+++ model/CommentsModel.class.php
@@ -1,11 +1,21 @@
 <?php
 
+require_once 'Model.class.php';
+
 class CommentsModel extends Model
 {
-    // Mengambil semua komentar yang dibuat oleh user tertentu
+    // // Mengambil semua komentar yang dibuat oleh user tertentu
     public function getCommentsByUserId($userId)
     {
-        $stmt = $this->db->prepare("SELECT * FROM comments WHERE id_user = ?");
+        $stmt = $this->db->prepare("
+        SELECT 
+            c.comment, 
+            c.id_comment, 
+            ct.name AS canteen_name
+        FROM comments c 
+        JOIN canteens ct ON c.id_canteen = ct.id_canteen
+        WHERE c.id_user = ?
+    ");
         $stmt->bind_param("i", $userId);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -20,17 +30,16 @@ class CommentsModel extends Model
     // Mengambil semua komentar pada sebuah kantin tertentu
     public function getCommentsByCanteenId($canteenId)
     {
-        $stmt = $this->db->prepare("SELECT * FROM comments WHERE id_canteen = ?");
+        $stmt = $this->db->prepare("SELECT comments.comment, users.name AS user_name
+            FROM comments JOIN users ON comments.id_user = users.id_user
+            WHERE comments.id_canteen = ?
+            ORDER BY comments.id_comment DESC");
         $stmt->bind_param("i", $canteenId);
         $stmt->execute();
         $result = $stmt->get_result();
-        $comments = [];
-        while ($row = $result->fetch_assoc()) {
-            $comments[] = $row;
-        }
-        $stmt->close();
-        return $comments;
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
+
 
     // Menambahkan komentar baru ke dalam database
     public function addComment($userId, $canteenId, $content)
@@ -50,5 +59,17 @@ class CommentsModel extends Model
         $result = $stmt->execute();
         $stmt->close();
         return $result;
+    }
+
+    public function getCanteenById($id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM canteens WHERE id_canteen = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+
+        return $row;
     }
 }
